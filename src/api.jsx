81f53f4ae605e741d062a9485979ca196d8a9c43
sumayya <sumayyahassainar37@gmail.com>
2025-11-01 21:02:59 +0530

    update api.jsx

--- src/api.jsx
+++ src/api.jsx
@@ -1,5 +1,9 @@
 import axios from "axios";
-const API = axios.create({ base_URL: "http://localhost:5000/api",withCredentials:true});
+
+const API = axios.create({
+  baseURL: "http://localhost:5000/api",
+  withCredentials: true,
+});
 
 // ✅ Attach token to all requests
 API.interceptors.request.use((req) => {
